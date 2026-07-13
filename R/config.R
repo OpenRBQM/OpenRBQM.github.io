296e5591a2baca8e81e5e4ffc652535d481e8f14
nandriychuk <natalia.andriychuk@gilead.com>
2026-07-13 17:06:27 -0400

    feat: pin Additional Packages to gh.dash, open.gismo, gsm.guide

--- R/config.R
+++ R/config.R
@@ -17,6 +17,7 @@ ext_pkgs        <- c("gsm.qtl", "gsm.viz", "clindata", "gsm.datasim")
 apps_pkgs       <- c("gsm.app", "gsm.digitpref", "gsm.ae", "gsm.query", "gsm.pd")
 qual_pkgs       <- c("qcthat", "gsm.qc", "gsm.utils")
 deprecated_pkgs <- c("gsm")
+additional_pkgs <- c("gh.dash", "open.gismo", "gsm.guide")
 
 # Flat package order for the Vignettes tab (mirrors the Packages tab order).
 curated_order <- c(core_pkgs, ext_pkgs, apps_pkgs, qual_pkgs)
